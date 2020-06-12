f8c0085fec59f88a72d684e2467a4a371cd7a3d5
Adam Holloway <adam.holloway21@gmail.com>
2020-06-12 21:34:13 +0100

    add redux and alert reducers and action

---
+++ client/src/actions/alert.js
@@ -0,0 +1,12 @@
+import uuid from 'uuid';
+import { SET_ALERT, REMOVE_ALERT } from './types';
+
+// Dispatch multiple actions
+export const setAlert = (message, alertType) => (dispatch) => {
+  const id = uuid.v4();
+
+  dispatch({
+    type: SET_ALERT,
+    payload: { msg, alertType, id },
+  });
+};
